e3bc789dc310b679ee5705370d2dae834a454305
Angel Brambila <malthael.91@gmail.com>
2020-09-15 07:04:52 -0500

    Try Catch , Casting and Functions

---
+++ Functions/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using static System.Console;
+
+namespace Functions
+{
+    class Program
+    {
+        #region EZ Functions    
+        // regime , modifers, return type, name , (parameters)
+        /// <summary>
+        /// This functions is used to get the Multiplication Table of a number
+        /// Also don't call it with an instance , it's static ￣へ￣
+        /// </summary>
+        /// <param name="number">Its a byte so don't push over 255</param>
+        static void TimesTable (byte number)
+        {
+            for (int row = 1; row < 10; row++)
+            {
+                WriteLine($"{row} x {number} = {row * number}");
+            }
+            WriteLine();
+        }
+
+        static void RunTimesTable()
+        {
+            bool isNumber;
+            do
+            {
+                Write ("Enter a number between 0 and 255: ");
+                isNumber = byte.TryParse(ReadLine(), out byte number);
+
+                if(isNumber)
+                {
+                    TimesTable(number);
+                }
+                else
+                {
+                    WriteLine("That's not a number m8");
+                }
+            } while (isNumber);
+        }
+        // DRY
+        // Don't Repeat Yourself
+        // KISS
+        // 
+        #endregion
+        
+        #region Return
+        static decimal returnSomething()
+        {
+            return 90.2M;
+        }
+        #endregion
+        
+        #region Factorial
+            static int Factorial(int number)
+            {
+                if (number < 1)
+                {
+                    return 0;
+                }
+                else if(number == 1)
+                {
+                    return 1;
+                }
+                else
+                {
+                return number * Factorial (number - 1);
+                }
+            }
+        #endregion
+        static void Main(string[] args)
+        {
+            RunTimesTable();
+            WriteLine(Factorial(5));
+            char a = '$';
+            int b = a;
+            WriteLine($"{b:X}");
+        }
+    }
+}
---
+++ HandlingExceptions/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using static System.Console;
+namespace HandlingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Managing Exceptions
+                WriteLine("Yur age please");
+                string input2 = ReadLine();
+                int anotherAge = 0;
+
+                try
+                {
+                anotherAge = int.Parse(input2);
+                WriteLine($"Good to be {anotherAge} years old.");
+                }
+                catch (FormatException)
+                {
+                    WriteLine("I need you to type your age in number. I.E: 89");
+                }
+                catch (OverflowException)
+                {
+                    WriteLine("Yamete Kudazai");
+                }
+                catch (Exception ex)
+                {
+                    WriteLine($"{ex.GetType()} says {ex.Message}");
+                }
+            #endregion
+
+            #region Checked
+
+            try
+            {
+            checked
+            {
+                int x = int.MaxValue -1;
+                WriteLine(x);
+                x++;
+                WriteLine($"After incrementing : {x:N0}");
+                x++;
+                WriteLine($"After incrementing : {x:N0}");
+                x++;
+                WriteLine($"After incrementing : {x:N0}");
+            }
+            }
+            catch (OverflowException)
+            {
+                WriteLine("The code overflowed but i got your back BRUH o(*￣▽￣*)ブ");
+            }
+            #endregion
+        }
+    }
+}
