412bf46b5e0f7b81198b4a1f73cfe9d2014fca38
Angel Brambila <malthael.91@gmail.com>
2020-11-06 10:53:54 -0600

    Cryptography pt2

--- Encryption/CryptoLib/Protector.cs
+++ Encryption/CryptoLib/Protector.cs
@@ -9,8 +9,16 @@ using static System.Convert;
 
 namespace CryptoLib
 {
-    public class Protector
+    public class User
     {
+        public string Name { get; set; }
+        public string Salt { get; set; }
+        public string SaltedHashedPassword { get; set; }
+    }
+    public static class Protector
+    {
+        #region Theory
+            
         /*
         Encryption and Decryption : Two-way process to convert data
         Hash : One-way process
@@ -19,7 +27,9 @@ namespace CryptoLib
         Authorization : Ensure to someones has permission to do something.
         Types of encryption : Symmetric and Asymmetric
         */
+        #endregion
 
+        #region Encrypt   
         // salt must be at least 8 bytes
         private static readonly byte[] salt = Encoding.Unicode.GetBytes("ElPepe");
         private static readonly int iterations = 2000;
@@ -44,7 +54,9 @@ namespace CryptoLib
 
             return Convert.ToBase64String(encryptedBytes);
         }
+        #endregion
 
+        #region Decrypt
         public static string Decrypt ( string cryptoText, string password)
         {
             byte [] plainBytes;
@@ -64,7 +76,48 @@ namespace CryptoLib
             }
             return Encoding.Unicode.GetString(plainBytes);
         }
+        #endregion
 
-    
+        private static Dictionary<string, User> Users = new Dictionary<string, User>();
+
+        public static User Register(string username, string password)
+        {
+            // generate random salt
+            var rng = RandomNumberGenerator.Create();
+            var saltBytes = new byte [16];
+            rng.GetBytes(saltBytes);
+            var saltText = Convert.ToBase64String(saltBytes);
+
+            // generate salted password
+            var saltedhashedPassword = SaltAndHashPassword(password, saltText);
+
+            var user = new User 
+            {
+                Name = username,
+                Salt = saltText,
+                SaltedHashedPassword = saltedhashedPassword
+            };
+            Users.Add(user.Name, user);
+            return user;
+        }
+
+        public static bool CheckPassword(string username, string password)
+        {
+            if(!Users.ContainsKey(username))
+            {
+                return false;
+            }
+            var user = Users[username];
+            // re-generate the same salted and same hashed password
+            var saltedhashedPassword = SaltAndHashPassword(password, user.Salt);
+            return (saltedhashedPassword == user.SaltedHashedPassword);
+        }
+
+        private static string SaltAndHashPassword(string password, string saltText)
+        {
+            var sha = SHA256.Create();
+            var saltedPassword = password + salt;
+            return Convert.ToBase64String(sha.ComputeHash(Encoding.Unicode.GetBytes(saltedPassword)));
+        }
     }
 }
---
+++ Encryption/Hashingapp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using static System.Console;
+using CryptoLib;
+
+namespace Hashingapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Registering Teisel UwU with Pa$$w0rd");
+            var teisel = Protector.Register("Teisel", "Pa$$w0rd");
+            WriteLine($"Name {teisel.Name}");
+            WriteLine($"Salt : {teisel.Salt}");
+            WriteLine($"Password (Salted and hashed) :  {teisel.SaltedHashedPassword}");
+
+            Write ("Enter a new user to register : ");
+            string username = ReadLine();
+            Write ($"Enter password for {username}");
+            string password = ReadLine();
+            var user = Protector.Register(username, password);
+            WriteLine($"Name {user.Name}");
+            WriteLine($"Salt : {user.Salt}");
+            WriteLine($"Password (Salted and hashed) :  {user.SaltedHashedPassword}");
+
+            WriteLine();
+
+            bool correctPassword = false;
+            while (!correctPassword)
+            {
+                Write("Enter a username to log in");
+                string loginUsername = ReadLine();
+                Write("Enter a password to log in");
+                string loginPassword = ReadLine();
+
+                correctPassword = Protector.CheckPassword(loginUsername, loginPassword);
+
+                    if(correctPassword)
+                    {
+                        WriteLine($"Welcome back! {loginUsername} has been logged in");
+                    }
+                    else
+                    {
+                        WriteLine("Invalid username or password");
+                    }
+            }
+
+        }
+    }
+}
